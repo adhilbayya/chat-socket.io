8be4af945747e355ad2890d4c8fd9a2356ed12d1
AdhilBayya <adhilvk445@gmail.com>
2024-08-27 13:48:19 +0530

    Added the server changes

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
   console.log(`user ${socket.id} connected`);
-  socket.on("message", (message) => {
-    console.log(message);
-    io.emit("message", `${socket.id.substring(0, 5)}: ${message}`);
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
   });
 });
 
