dfb8e7b75454c11480debe637155cd202c5b025c
AdhilBayya <adhilvk445@gmail.com>
2024-09-10 19:04:52 +0530

    Ready for deployment

--- server/public/app.js
+++ server/public/app.js
@@ -1,4 +1,4 @@
-const socket = io("https://chat-socket-io-0o7t.onrender.com/");
+const socket = io("https://chat-socket-io-0fvj.onrender.com/");
 
 const activity = document.querySelector(".activity");
 const msgInput = document.querySelector("#message");
