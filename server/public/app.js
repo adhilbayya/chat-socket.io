cfe0bdf9b295495b50bdad4f893b9240d107f4d1
AdhilBayya <adhilvk445@gmail.com>
2024-09-10 15:29:02 +0530

    Added the frontend

--- server/public/app.js
+++ server/public/app.js
@@ -36,9 +36,26 @@ document.querySelector(".form-send").addEventListener("submit", sendMessage);
 
 socket.on("message", (data) => {
   activity.textContent = "";
+  const { name, text, time } = data;
   const li = document.createElement("li");
-  li.textContent = data;
-  document.querySelector("ul").appendChild(li);
+  li.className = "post";
+  if (name === nameInput.value) li.className = "post post--left";
+  if (name !== nameInput.value && name !== "Admin")
+    li.className = "post post--right";
+
+  if (name === "Admin") {
+    li.innerHTML = `<div class="post--header ${
+      name === nameInput.value ? "post--header--user" : "post--header--reply"
+    }">
+      <span class="post--header--name">${name}</span>
+      <span class="post--header--time">${time}</span>
+    </div>
+    <div class="post--text">${text}</div>`;
+  } else {
+    li.innerHTML = `<div class="post--text">${text}</div>`;
+  }
+  document.querySelector(".chat-display").appendChild(li);
+  chatDisplay.scrollTop = chatDisplay.scrollHeight;
 });
 
 input.addEventListener("keypress", () => {
@@ -55,3 +72,29 @@ socket.on("activity", (name) => {
     activity.textContent = "";
   }, 1000);
 });
+
+const showUsers = (users) => {
+  usersList.textContent = "";
+  if (users) {
+    usersList.innerHTML = `<em>Users in ${roomId.value}:</em>`;
+    users.forEach((user, i) => {
+      usersList.textContent += ` ${user.name}`;
+      if (users.length > 1 && i !== users.length - 1) {
+        usersList.textContent += ",";
+      }
+    });
+  }
+};
+
+const showRooms = (rooms) => {
+  roomList.textContent = "";
+  if (rooms) {
+    roomList.innerHTML = `<em>Active rooms:</em>`;
+    rooms.forEach((room, i) => {
+      roomList.textContent += ` ${room}`;
+      if (rooms.length > 1 && i !== rooms.length - 1) {
+        roomList.textContent += ",";
+      }
+    });
+  }
+};
